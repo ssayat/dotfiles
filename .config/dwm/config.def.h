98b8d81f2ec71444fc1ea0094a9a1fd583a02bb4
Sayat Sembekov <ssayat@icloud.com>
2023-09-13 23:53:54 +0600

    Disable the systray for dwm

--- .config/dwm/config.def.h
+++ .config/dwm/config.def.h
@@ -14,7 +14,7 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#44454a";
 
-static const int showsystray = 1; /* 0 means no systray */
+static const int showsystray = 0; /* 0 means no systray */
 static const int systrayonleft = 0; /* 0 for right, 1 for left */
 static const int systraypinning = 0; /* 0 for default behavior */
 static const int systraypinningfailfirst = 0; /* 0 for default behavior */
