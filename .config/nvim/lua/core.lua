7781d9393b56aa566ed2891a8e16dc7483a0ffec
Sayat Sembekov <ssayat@icloud.com>
2023-10-29 23:39:09 +0600

    Update neovim config

--- .config/nvim/lua/core.lua
+++ .config/nvim/lua/core.lua
@@ -38,27 +38,6 @@ require'nvim-treesitter.configs'.setup {
   },
 }
 
--- disable netrw at the very start of your init.lua
-vim.g.loaded_netrw = 1
-vim.g.loaded_netrwPlugin = 1
-
--- set termguicolors to enable highlight groups
-vim.opt.termguicolors = true
-
--- OR setup with some options
-require("nvim-tree").setup({
-  sort_by = "case_sensitive",
-  view = {
-    width = 30,
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = false,
-  },
-})
-
 require('leap').add_default_mappings()
 
 vim.g.loaded_netrw = 1
